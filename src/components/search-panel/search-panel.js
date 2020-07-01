bdfab96d83d8e1e72e045ac584c237e077786abd
Bohdan <bogdanpaseka2000@gmail.com>
2020-07-01 13:32:01 +0300

    React-project was finished

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,27 @@
+import React , {Component} from 'react'
+import './search-panel.css'
+export default class SearchPanel extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            tern : ''
+        }
+        this.onUpdateSearch = this.onUpdateSearch.bind(this)
+    }
+    onUpdateSearch(e){
+        const tern = e.target.value;
+        this.setState ({tern})
+
+        this.props.onUpdateSearch(tern)
+    }
+    render (){
+        return (
+            <input 
+                className = "form-control search-input "
+                type="text"
+                placeholder="Searching in entries"
+                onChange = {this.onUpdateSearch}
+            />
+        )
+    }
+}
